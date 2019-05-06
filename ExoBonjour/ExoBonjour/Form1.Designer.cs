d3fbb7a94b6130dc5595df0da7b4474d31b0ca7c
PARDO Christopher <Christopher.PARDO@cpnv.ch>
2019-05-06 15:57:25 +0200

    BonjourQui?

---
+++ ExoBonjour/ExoBonjour/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ExoBonjour
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.blQuestion = new System.Windows.Forms.Label();
+            this.blName = new System.Windows.Forms.TextBox();
+            this.btBienvenue = new System.Windows.Forms.Button();
+            this.rp = new System.Windows.Forms.Label();
+            this.btCroix = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // blQuestion
+            // 
+            this.blQuestion.AutoSize = true;
+            this.blQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blQuestion.Location = new System.Drawing.Point(53, 59);
+            this.blQuestion.Name = "blQuestion";
+            this.blQuestion.Size = new System.Drawing.Size(107, 24);
+            this.blQuestion.TabIndex = 0;
+            this.blQuestion.Text = "Votre nom";
+            this.blQuestion.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // blName
+            // 
+            this.blName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blName.Location = new System.Drawing.Point(185, 59);
+            this.blName.Name = "blName";
+            this.blName.Size = new System.Drawing.Size(100, 29);
+            this.blName.TabIndex = 1;
+            this.blName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btBienvenue
+            // 
+            this.btBienvenue.Location = new System.Drawing.Point(160, 178);
+            this.btBienvenue.Name = "btBienvenue";
+            this.btBienvenue.Size = new System.Drawing.Size(141, 75);
+            this.btBienvenue.TabIndex = 2;
+            this.btBienvenue.Text = "Bienvenue";
+            this.btBienvenue.UseVisualStyleBackColor = true;
+            this.btBienvenue.Click += new System.EventHandler(this.btBienvenue_Click);
+            // 
+            // rp
+            // 
+            this.rp.AutoSize = true;
+            this.rp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rp.Location = new System.Drawing.Point(65, 134);
+            this.rp.Name = "rp";
+            this.rp.Size = new System.Drawing.Size(24, 20);
+            this.rp.TabIndex = 3;
+            this.rp.Text = "...";
+            this.rp.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // btCroix
+            // 
+            this.btCroix.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btCroix.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.btCroix.Image = global::ExoBonjour.Properties.Resources.delete;
+            this.btCroix.Location = new System.Drawing.Point(436, 200);
+            this.btCroix.Name = "btCroix";
+            this.btCroix.Size = new System.Drawing.Size(79, 77);
+            this.btCroix.TabIndex = 4;
+            this.btCroix.UseVisualStyleBackColor = true;
+            this.btCroix.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btCroix);
+            this.Controls.Add(this.rp);
+            this.Controls.Add(this.btBienvenue);
+            this.Controls.Add(this.blName);
+            this.Controls.Add(this.blQuestion);
+            this.Name = "Form1";
+            this.Text = "Bonjour Qui ?";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label blQuestion;
+        private System.Windows.Forms.TextBox blName;
+        private System.Windows.Forms.Button btBienvenue;
+        private System.Windows.Forms.Label rp;
+        private System.Windows.Forms.Button btCroix;
+    }
+}
+
---
+++ ExoBonjour/ExoBonjour/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExoBonjour
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (blName.Text == "")
+            {
+                btBienvenue.Enabled = false;
+            }
+            else
+            {
+                btBienvenue.Enabled = true;
+            }
+        }
+
+        private void btBienvenue_Click(object sender, EventArgs e)
+        {
+            rp.Text = "Bonjour " + blName.Text;
+            blName.Text = "";
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
