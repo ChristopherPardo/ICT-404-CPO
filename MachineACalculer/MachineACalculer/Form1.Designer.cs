5087c716005408225a2df8422249ab16cc1fc050
PARDO Christopher <Christopher.PARDO@cpnv.ch>
2019-06-13 09:01:17 +0200

    Machine a calculer partie graphique

---
+++ MachineACalculer/MachineACalculer/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace MachineACalculer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BT_1 = new System.Windows.Forms.Button();
+            this.BT_2 = new System.Windows.Forms.Button();
+            this.BT_3 = new System.Windows.Forms.Button();
+            this.BT_4 = new System.Windows.Forms.Button();
+            this.BT_5 = new System.Windows.Forms.Button();
+            this.BT_6 = new System.Windows.Forms.Button();
+            this.BT_7 = new System.Windows.Forms.Button();
+            this.BT_8 = new System.Windows.Forms.Button();
+            this.BT_9 = new System.Windows.Forms.Button();
+            this.BT_0 = new System.Windows.Forms.Button();
+            this.BT_Decimal = new System.Windows.Forms.Button();
+            this.BT_Clean = new System.Windows.Forms.Button();
+            this.BT_Res = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.BT_Add = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BT_Sous = new System.Windows.Forms.Button();
+            this.BT_Multi = new System.Windows.Forms.Button();
+            this.TB_Chiffre1 = new System.Windows.Forms.Label();
+            this.TB_Chiffre2 = new System.Windows.Forms.Label();
+            this.TB_Resultat = new System.Windows.Forms.Label();
+            this.BT_Div = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BT_1
+            // 
+            this.BT_1.Location = new System.Drawing.Point(64, 22);
+            this.BT_1.Name = "BT_1";
+            this.BT_1.Size = new System.Drawing.Size(43, 43);
+            this.BT_1.TabIndex = 0;
+            this.BT_1.Text = "1";
+            this.BT_1.UseVisualStyleBackColor = true;
+            // 
+            // BT_2
+            // 
+            this.BT_2.Location = new System.Drawing.Point(146, 22);
+            this.BT_2.Name = "BT_2";
+            this.BT_2.Size = new System.Drawing.Size(43, 43);
+            this.BT_2.TabIndex = 1;
+            this.BT_2.Text = "2";
+            this.BT_2.UseVisualStyleBackColor = true;
+            // 
+            // BT_3
+            // 
+            this.BT_3.Location = new System.Drawing.Point(226, 23);
+            this.BT_3.Name = "BT_3";
+            this.BT_3.Size = new System.Drawing.Size(43, 43);
+            this.BT_3.TabIndex = 2;
+            this.BT_3.Text = "3";
+            this.BT_3.UseVisualStyleBackColor = true;
+            // 
+            // BT_4
+            // 
+            this.BT_4.Location = new System.Drawing.Point(64, 78);
+            this.BT_4.Name = "BT_4";
+            this.BT_4.Size = new System.Drawing.Size(43, 43);
+            this.BT_4.TabIndex = 3;
+            this.BT_4.Text = "4";
+            this.BT_4.UseVisualStyleBackColor = true;
+            // 
+            // BT_5
+            // 
+            this.BT_5.Location = new System.Drawing.Point(146, 78);
+            this.BT_5.Name = "BT_5";
+            this.BT_5.Size = new System.Drawing.Size(43, 43);
+            this.BT_5.TabIndex = 4;
+            this.BT_5.Text = "5";
+            this.BT_5.UseVisualStyleBackColor = true;
+            // 
+            // BT_6
+            // 
+            this.BT_6.Location = new System.Drawing.Point(226, 78);
+            this.BT_6.Name = "BT_6";
+            this.BT_6.Size = new System.Drawing.Size(43, 43);
+            this.BT_6.TabIndex = 6;
+            this.BT_6.Text = "6";
+            this.BT_6.UseVisualStyleBackColor = true;
+            // 
+            // BT_7
+            // 
+            this.BT_7.Location = new System.Drawing.Point(64, 135);
+            this.BT_7.Name = "BT_7";
+            this.BT_7.Size = new System.Drawing.Size(43, 43);
+            this.BT_7.TabIndex = 7;
+            this.BT_7.Text = "7";
+            this.BT_7.UseVisualStyleBackColor = true;
+            // 
+            // BT_8
+            // 
+            this.BT_8.Location = new System.Drawing.Point(146, 135);
+            this.BT_8.Name = "BT_8";
+            this.BT_8.Size = new System.Drawing.Size(43, 43);
+            this.BT_8.TabIndex = 8;
+            this.BT_8.Text = "8";
+            this.BT_8.UseVisualStyleBackColor = true;
+            // 
+            // BT_9
+            // 
+            this.BT_9.Location = new System.Drawing.Point(226, 137);
+            this.BT_9.Name = "BT_9";
+            this.BT_9.Size = new System.Drawing.Size(43, 43);
+            this.BT_9.TabIndex = 9;
+            this.BT_9.Text = "9";
+            this.BT_9.UseVisualStyleBackColor = true;
+            // 
+            // BT_0
+            // 
+            this.BT_0.Location = new System.Drawing.Point(146, 193);
+            this.BT_0.Name = "BT_0";
+            this.BT_0.Size = new System.Drawing.Size(43, 43);
+            this.BT_0.TabIndex = 10;
+            this.BT_0.Text = "0";
+            this.BT_0.UseVisualStyleBackColor = true;
+            // 
+            // BT_Decimal
+            // 
+            this.BT_Decimal.Location = new System.Drawing.Point(64, 193);
+            this.BT_Decimal.Name = "BT_Decimal";
+            this.BT_Decimal.Size = new System.Drawing.Size(43, 43);
+            this.BT_Decimal.TabIndex = 11;
+            this.BT_Decimal.Text = ".";
+            this.BT_Decimal.UseVisualStyleBackColor = true;
+            // 
+            // BT_Clean
+            // 
+            this.BT_Clean.Location = new System.Drawing.Point(64, 266);
+            this.BT_Clean.Name = "BT_Clean";
+            this.BT_Clean.Size = new System.Drawing.Size(43, 43);
+            this.BT_Clean.TabIndex = 13;
+            this.BT_Clean.Text = "C";
+            this.BT_Clean.UseVisualStyleBackColor = true;
+            // 
+            // BT_Res
+            // 
+            this.BT_Res.Location = new System.Drawing.Point(146, 266);
+            this.BT_Res.Name = "BT_Res";
+            this.BT_Res.Size = new System.Drawing.Size(123, 43);
+            this.BT_Res.TabIndex = 14;
+            this.BT_Res.Text = "=";
+            this.BT_Res.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(226, 193);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(43, 43);
+            this.button14.TabIndex = 16;
+            this.button14.Text = "%";
+            this.button14.UseVisualStyleBackColor = true;
+            // 
+            // BT_Add
+            // 
+            this.BT_Add.Location = new System.Drawing.Point(334, 23);
+            this.BT_Add.Name = "BT_Add";
+            this.BT_Add.Size = new System.Drawing.Size(43, 60);
+            this.BT_Add.TabIndex = 17;
+            this.BT_Add.Text = "+";
+            this.BT_Add.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 357);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Chiffre 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(331, 357);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Chiffre 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(158, 412);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Total";
+            // 
+            // BT_Sous
+            // 
+            this.BT_Sous.Location = new System.Drawing.Point(334, 100);
+            this.BT_Sous.Name = "BT_Sous";
+            this.BT_Sous.Size = new System.Drawing.Size(43, 60);
+            this.BT_Sous.TabIndex = 22;
+            this.BT_Sous.Text = "-";
+            this.BT_Sous.UseVisualStyleBackColor = true;
+            // 
+            // BT_Multi
+            // 
+            this.BT_Multi.Location = new System.Drawing.Point(334, 176);
+            this.BT_Multi.Name = "BT_Multi";
+            this.BT_Multi.Size = new System.Drawing.Size(43, 60);
+            this.BT_Multi.TabIndex = 23;
+            this.BT_Multi.Text = "*";
+            this.BT_Multi.UseVisualStyleBackColor = true;
+            // 
+            // TB_Chiffre1
+            // 
+            this.TB_Chiffre1.AutoSize = true;
+            this.TB_Chiffre1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.TB_Chiffre1.Location = new System.Drawing.Point(143, 357);
+            this.TB_Chiffre1.Name = "TB_Chiffre1";
+            this.TB_Chiffre1.Size = new System.Drawing.Size(35, 13);
+            this.TB_Chiffre1.TabIndex = 24;
+            this.TB_Chiffre1.Text = "label4";
+            // 
+            // TB_Chiffre2
+            // 
+            this.TB_Chiffre2.AutoSize = true;
+            this.TB_Chiffre2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.TB_Chiffre2.Location = new System.Drawing.Point(402, 357);
+            this.TB_Chiffre2.Name = "TB_Chiffre2";
+            this.TB_Chiffre2.Size = new System.Drawing.Size(35, 13);
+            this.TB_Chiffre2.TabIndex = 25;
+            this.TB_Chiffre2.Text = "label5";
+            // 
+            // TB_Resultat
+            // 
+            this.TB_Resultat.AutoSize = true;
+            this.TB_Resultat.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.TB_Resultat.Location = new System.Drawing.Point(206, 412);
+            this.TB_Resultat.Name = "TB_Resultat";
+            this.TB_Resultat.Size = new System.Drawing.Size(35, 13);
+            this.TB_Resultat.TabIndex = 26;
+            this.TB_Resultat.Text = "label6";
+            // 
+            // BT_Div
+            // 
+            this.BT_Div.Location = new System.Drawing.Point(334, 249);
+            this.BT_Div.Name = "BT_Div";
+            this.BT_Div.Size = new System.Drawing.Size(43, 60);
+            this.BT_Div.TabIndex = 27;
+            this.BT_Div.Text = "/";
+            this.BT_Div.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BT_Div);
+            this.Controls.Add(this.TB_Resultat);
+            this.Controls.Add(this.TB_Chiffre2);
+            this.Controls.Add(this.TB_Chiffre1);
+            this.Controls.Add(this.BT_Multi);
+            this.Controls.Add(this.BT_Sous);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BT_Add);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.BT_Res);
+            this.Controls.Add(this.BT_Clean);
+            this.Controls.Add(this.BT_Decimal);
+            this.Controls.Add(this.BT_0);
+            this.Controls.Add(this.BT_9);
+            this.Controls.Add(this.BT_8);
+            this.Controls.Add(this.BT_7);
+            this.Controls.Add(this.BT_6);
+            this.Controls.Add(this.BT_5);
+            this.Controls.Add(this.BT_4);
+            this.Controls.Add(this.BT_3);
+            this.Controls.Add(this.BT_2);
+            this.Controls.Add(this.BT_1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BT_1;
+        private System.Windows.Forms.Button BT_2;
+        private System.Windows.Forms.Button BT_3;
+        private System.Windows.Forms.Button BT_4;
+        private System.Windows.Forms.Button BT_5;
+        private System.Windows.Forms.Button BT_6;
+        private System.Windows.Forms.Button BT_7;
+        private System.Windows.Forms.Button BT_8;
+        private System.Windows.Forms.Button BT_9;
+        private System.Windows.Forms.Button BT_0;
+        private System.Windows.Forms.Button BT_Decimal;
+        private System.Windows.Forms.Button BT_Clean;
+        private System.Windows.Forms.Button BT_Res;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button BT_Add;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button BT_Sous;
+        private System.Windows.Forms.Button BT_Multi;
+        private System.Windows.Forms.Label TB_Chiffre1;
+        private System.Windows.Forms.Label TB_Chiffre2;
+        private System.Windows.Forms.Label TB_Resultat;
+        private System.Windows.Forms.Button BT_Div;
+    }
+}
+
